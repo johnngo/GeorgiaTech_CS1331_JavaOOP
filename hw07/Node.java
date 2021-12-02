9df68412ebe4d87c5733ecc2bc34736612f14fce
John Ngo <ngongo_jn@hotmail.com>
2021-12-02 14:44:58 -0800

    change file

--- hw07/Node.java
+++ hw07/Node.java
@@ -1,6 +1,9 @@
 class Node<E> {
+    //a variable named data
     public E data;
+    //a variable named next
     public Node <E> next;
+    //constructor, takes in two arguments exactly in the given order-data and next node and assigns it
     Node(E data, Node<E> next) {
         this.data = data;
         this.next = next;
@@ -8,12 +11,15 @@ class Node<E> {
     Node(E data) {
         this(data,null);
     }
+
+    //methods//getters and setters for instance variables
     public E getData() {
         return data;
     }
     public Node<E> getNodeNext() {
         return next;
     }
+    //setters for instance variables
     public void setData(E newData){
         data = newData;
     }
